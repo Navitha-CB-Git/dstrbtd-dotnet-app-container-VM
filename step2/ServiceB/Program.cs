09596cde31d03260cc9fe133246e2d5613d191e8
Denis Ivanov <deivanov@microsoft.com>
2022-11-07 16:00:29 -0800

    Step2

---
+++ step2/ServiceB/Program.cs
@@ -0,0 +1,31 @@
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder => tracerProviderBuilder
+    .ConfigureResource(resourceBuilder => resourceBuilder.AddService("ServiceB"))
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .AddJaegerExporter(options => options.ExportProcessorType = ExportProcessorType.Simple)
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
